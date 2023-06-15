e8fce7e254cef1e72b1bc65e92293deb822e604c
Dev <zainulhassan1018@gmail.com>
2023-06-15 09:26:55 -0700

    Zain: Integerated OpenAI

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const userPrompt = require("./UserInterface/interface")
 const express = require('express')
 const app = express()
 
-app.listen('3000', () => {
-    console.log('Listening on port 3000.')
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+    userPrompt
 })
