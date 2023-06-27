e9761086e1369f1e690af6dbea18ac680305669b
Dev <zainulhassan1018@gmail.com>
2023-06-27 04:43:45 -0700

    Zain: Connected to mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const userPrompt = require("./UserInterface/interface")
 const express = require('express')
 const app = express()
 
+const connectDb = require("./config/database")
+connectDb()
+
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`)
     userPrompt
